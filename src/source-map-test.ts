6021d585c943bd209b67e5f2f1dd090647e19d38
YuYokoyama <yu.yokoyama@sportsbull.jp>
2022-11-23 13:06:19 +0900

    feat: source map

--- src/source-map-test.ts
+++ src/source-map-test.ts
@@ -3,3 +3,9 @@ const message: string = 'hello';
 console.log(message);
 console.log(`${message}2`);
 console.log(`${message}3`);
+
+function hello(): void {
+    console.log('hello in function');
+}
+
+hello();
